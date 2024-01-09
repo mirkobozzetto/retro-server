41170d19774589f447426948a611a804d6e08cc8
Mirko Bozzetto <mirkouch@gmail.com>
2024-01-09 10:31:34 +0100

    Add id field to Aliment schema and update CRUD operations
    This commit adds a new "id" field to the Aliment schema in the "foods.ts" file. It also updates the CRUD operations to include the new field.

--- src/db/foods.ts
+++ src/db/foods.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const SchemaAliment = new mongoose.Schema({
+  id: { type: String, required: true },
   aliment: { type: String, required: true },
   calories: { type: Number, required: true },
   vitamines: { type: String, required: true },
@@ -10,4 +11,17 @@ const SchemaAliment = new mongoose.Schema({
   graisses: { type: String, required: true },
 });
 
-export const Aliment = mongoose.model("Aliment", SchemaAliment);
+export const AlimentModel = mongoose.model("Aliment", SchemaAliment);
+export const getAliments = () => AlimentModel.find();
+
+export const alimentType = (aliment: string) => AlimentModel.findById(aliment);
+export const alimentById = (id: string) => AlimentModel.findById(id);
+
+export const createAliment = (values: Record<string, unknown>) =>
+  new AlimentModel(values).save().then((aliment) => aliment.toObject());
+
+export const deleteAlimentbyId = (id: string) =>
+  AlimentModel.findByIdAndDelete({ _id: id });
+
+export const updateAliment = (id: string, values: Record<string, unknown>) =>
+  AlimentModel.findByIdAndUpdate(id, values, { new: true });
